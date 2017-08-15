933eb3dff4ef423c44e8433f6bc4ea818f3cfeae
Dmitry Ananichev <a@qozz.ru>
2017-08-15 16:55:57 +0500

    container name changed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name bitcoin-testbox -tid -p 19001:19001 cryptochain/bitcoin-regtest:latest
+docker run --name bitcoin-regtest -tid -p 19001:19001 cryptochain/bitcoin-regtest:latest
